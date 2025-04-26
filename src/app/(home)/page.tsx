cc5983c03dba737e1a5456e824e866c168fb1668
Yao Zu <1796381757@qq.com>
2025-04-26 22:02:17 +0800

    添加模板画廊组件，更新模板常量以支持多种文档类型

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 
 import { Navbar } from "./navbar";
+import { TemplatesGallery } from "./templates-gallery";
+
 const Home = () => {
   return (
     <div className="flex min-h-screen flex-col">
@@ -8,11 +10,7 @@ const Home = () => {
         <Navbar />
       </div>
       <div className="mt-16">
-        Click
-        <Link href="/documents/123">
-          <span className="text-blue-500 underline">&nbsp;here&nbsp;</span>
-        </Link>
-        to go to document id.
+        <TemplatesGallery />
       </div>
     </div>
   );
